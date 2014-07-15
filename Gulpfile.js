8db527804f8dabe8b2579073de57262b048cf162
Justin Clune <jclune.berkley@gmail.com>
2014-07-15 19:30:53 +0900

    views+routing #5, matching.jade #7,
    'fix routing after rebase'

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,44 @@
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-// var mocha  = require('gulp-mocha');
+var mocha  = require('gulp-mocha');
 
-function lint() {
-  return gulp
-    .src(['Gulpfile.js', 'public/scripts/*.js', 'routes/*.js', 'test/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));  
+function handleError(err) {
+  console.log('gulp error ', err.toString());
+  this.emit('end');
 }
 
-gulp.task('lint', function() {
-  return lint();
+gulp.task('lint', function(cb) {
+  gulp
+    .src(['Gulpfile.js', 'public/scripts/*.js', 'routes/*.js', 'test/*.js'])
+    .pipe(jshint()
+      .on('error', handleError))
+    .pipe(jshint.reporter(stylish));
+  cb(null);
 });
 
-gulp.task('default', ['lint'], function() {
-  lint();
-  gulp.watch(['Gulpfile.js', 'public/scripts/*.js', 'routes/*.js','test/*.js'], function() {
-    console.log('gulp is watching!');
-    return lint();
-  });
-  console.log('gulp is watching!');
+//unit tests (instead of nyan cat 'spec' report is also OK)
+gulp.task('mocha', function(cb){
+  gulp
+    .src(['test/test.js'], {read: false})
+    .pipe(mocha({reporter: 'nyan'})
+      .on('error', handleError));
+  cb(null);
 });
+
+var tester = gulp.series('mocha', 'lint');
+
+gulp.task('default', function(cb) {
+  gulp.run(tester);
+  cb(null);
+});
+
+gulp.watch(['Gulpfile.js', 'public/scripts/*.js', 'routes/*.js','test/*.js'], ['default']);
+
+// must use gulp Beta. If not, do an asyncronous version:
+// gulp.task('default', ['lint', 'mocha'], function() {
+// });
+// var watcher = gulp.watch(['Gulpfile.js', 'public/scripts/*.js', 'routes/*.js','test/*.js'], tester);
+// watcher.on('change', function(event){
+//   console.log('File '+event.path+' was '+event.type+', running tasks...');
+// });
