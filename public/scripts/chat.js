3cb4b4744fdae575ea482fea57e90f3cc6e9ed39
Sakai Fuyuki <sakai_fuyuki@cyberagent.co.jp>
2014-07-17 16:18:54 +0900

    chat: get userId from session

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,4 +1,38 @@
-module.exports = function (io) {
+var Session = require('express-session').Session;
+
+module.exports = function (app, io) {
+
+  // to socket from session
+  io.use(function(socket, next) {
+    getSession(app, socket, function(err, session) {
+      if (err) next(err);
+      socket.client.session = session;
+      next();
+    });
+  });
+
+  var getSession = function(app, socket, callback) {
+    var sessionStore = app.get('store');
+    var s_cookie = require('express/node_modules/cookie').parse(socket.request.headers.cookie);
+    var cookie = require('cookie-parser/lib/parse').signedCookies(s_cookie, app.get('secret key'));
+    var sid = cookie['connect.sid'];
+
+    sessionStore.get(sid, function(err, sessionData) {
+      if(err || !sessionData) {
+        return callback(err ? err: 'Handshake failed');
+      }
+
+      var session = new Session({
+        sessionID: sid,
+        sessionStore: sessionStore
+      }, sessionData);
+
+      callback(null, session);
+    });
+  };
+
+  // chat
+
   var chatRoom = io.of('/chatRoom');
   var chatRoomList = io.of('/chatRoomList');
 
@@ -13,7 +47,9 @@ module.exports = function (io) {
     
     socket.on('message', function(message) {
       console.log(socket.id + ': ' + message);
-      chatRoom.to(socket.rooms[1]).emit('message', message);
+
+      var userId = socket.client.session.passport.user;
+      chatRoom.to(socket.rooms[1]).emit('message', {userId: userId, message: message});
     });
   });
 
