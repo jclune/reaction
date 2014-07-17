c37f33dec64ffbee46ccc72961af0ea7f50f2cb6
kou_hin <kou_hin@cyberagent.co.jp>
2014-07-17 15:52:21 +0900

    Fix #13: 自己紹介変更API

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,16 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
 var User = mongoose.model('User');
+var _s = require('underscore.string');
 
 /* GET users listing. */
 router.get('/', function (req, res) {
   res.send('respond with a resource');
 });
 
+/**
+ * 写真取得
+ */
 router.get('/:id([0-9a-f]{24})/picture', function (req, res) {
   var uid = req.params.id;
 
@@ -18,5 +22,45 @@ router.get('/:id([0-9a-f]{24})/picture', function (req, res) {
     return res.redirect('http://graph.facebook.com/' + u.facebook.id + '/picture');
   });
 });
+
+/**
+ * 自己紹介
+ */
+router.route('/:id([0-9a-f]{24})/bio')
+
+  .get(function (req, res) {
+    // 自己紹介の取得
+    var uid = req.params.id;
+    return User.findById(new ObjectId(uid), 'bio', function (err, u) {
+      if(err) throw err;
+      u = u || {};
+      return res.json({
+        bio: u.bio || ''
+      });
+    });
+  })
+  .put(function (req, res) {
+    // 自己紹介の更新
+    var uid = req.params.id;
+
+    // ユーザー権限チェック
+    var user = req.user;
+    if(uid != user.id) {
+      return res.send('no_auth');
+    }
+
+    // 内容があるかどうか
+    console.log(req.params);
+    console.log("HELLO", req.param('bio'));
+    if(!_s.trim(req.body.bio)) {
+      return res.send('bio_is_empty');
+    }
+
+    User.findByIdAndUpdate(new ObjectId(uid), {
+      bio: req.body.bio
+    }, function(err, u) {
+      return res.send('true');
+    });
+  });
 
 module.exports = router;
