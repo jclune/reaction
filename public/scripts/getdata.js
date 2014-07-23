a9c8eae20a13b48c3859cc39c4714d8a18877ff9
Justin Clune <jclune.berkley@gmail.com>
2014-07-23 11:37:52 +0900

    get team data in dragdrop.js for matching.jade (+2 squashed commits) Squashed commits: [8c641d5] unfinished matching process front end [6097ab3] added mockUser to getdata

--- public/scripts/getdata.js
+++ public/scripts/getdata.js
@@ -13,61 +13,81 @@
   getdata = {version: '0.0.0'};
 
   var socket = io.connect();
-  // hard coded friends for error
-  var friends = ['53c75703945748fab4769e23', '53c75703945748fab4769e24'];
-
-  // getdata.put = function(){
-
-  //   var prefix = 'my-';
-
-  //   $.ajax({
-  //     url: "/users/my/bio", 
-  //     data: "put my bio",
-  //     type: "PUT"
-  //   }).done(function(result){
-  //     console.log("Put result: ", result);
-  //   });
-  // };
+  // hard coded mock User
+  var mockUser = {
+    _id: "53c75703945748fab4769e22",
+    __v: 0,
+    bio: "I just wanna make friends :)",
+    email: "email@gmail.com",
+    facebook: {
+      bio: "“Life is like riding a bicycle. To keep your balance you must keep moving” -Albert Einstein",
+      birthday: "09/27/1988",
+      first_name: "First",
+      last_name: "Last",
+      gender: "male",
+      id: "10101353752266468",
+      link: "https://www.facebook.com/app_scoped_user_id/10101353752266468/",
+      locale: "ja_JP",
+      name: "First Last",
+      timezone: 9,
+      updated_time: "2014-07-19T05:29:17+0000",
+      verified: true
+    },
+    friends: ['53c75703945748fab4769e23', '53c75703945748fab4769e24'],
+    gender: "male",
+    name: "First Last",
+    provider: "facebook",
+    username: "username"
+  };
 
   getdata.get = function(){
 
     var prefix = 'my-';
 
-    socket.emit('user:info', function(data){
-      if (data.error){
-        console.log('socket error', data.error);
+    socket.emit('user:info', function(err, data){
+      console.log("socket started");
+      if (err){
+        console.log('socket error', err);
       }
-      setElements(data.user, prefix);
-      setFriends(data.user.friends, data.user._id);
+      else if (data){
+        console.log("my data", data);
+        setElements(data, prefix);
+        getFriends(data.friends);
+      } else {
+        console.log('use mock user object because no user exists');
+        setElements(mockUser, prefix);
+        getFriends(mockUser.friends);
+      }
     });
 
     setPicture('my', prefix);
   };
 
-  function setFriends(fids, uid){
+  function getFriends(fids){
     if (!fids){
-      fids = friends;
+      fids = mockUser.friends;
       console.log('use hard coded friends because no user.friends exist');
     }
 
-    fids.forEach(function(fid, i){
+    fids.forEach( function(fid, i){
 
       var prefix = 'f'+(i+1)+'-';
       setPicture(fid, prefix);
 
-      if ('user.friends havent logged in'){
-        fid = uid || 'couldnt get my id either';
-        console.log('use my data for all 3 because friends havent logged in', fid);
-      }
-
-      socket.emit('user:info', {id: fid}, function(data){
-        if (data.error){
-          console.log('socket error', data.error);
+      socket.emit('user:info', {id: fid}, function(err, data){
+        console.log('i=',i);
+        if (err){
+          console.log('socket error', err);
         }
-        console.log(data.user);
-        setElements(data.user, prefix);
+        if (data) {
+          console.log("friend "+prefix, data);
+          setElements(data, prefix);
+        } else {
+          console.log('use mock user object because no user exists');
+          setElements(mockUser, prefix);
+        }        
       });
-    }); 
+    });
   }
 
   function setPicture(fid, prefix){
@@ -87,28 +107,28 @@
   // finds html class names that match the user data keys
   function setElements(user, prefix){
     //console.log("socket data", data);
+
     var d = new Date();
     var age = parseInt((d.getTime() - Date.parse(user.facebook.birthday))/1000/60/60/24/365.25);
-
-    console.log(user);
 
     var data = {
       uid: user._id,
       name: user.name,
       bio: user.bio ||　user.facebook.bio,
       gender: user.gender || user.facebook.gender,
-      age: age
+      age: age,
+      className: user.fieldName
     };
 
-    for (var key in data){
+    for (var className in data){
       var my = prefix || '';
-      var elements = document.getElementsByClassName(my+key);
+      var elements = document.getElementsByClassName(my+className);
       if (elements){
         for (var i=0; i < elements.length; i++){
-          setElement(elements[i], validator.toString(data[key]));
+          setElement(elements[i], validator.toString(data[className]));
         }
       } else {
-        console.log("document doesn't have elments for: ", my+key);
+        console.log("document doesn't have elments for: ", my+className);
       }
     }
   }
